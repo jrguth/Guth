c7c9dc5eaf1fb09df7ea44436855cd77fa85d0ce
jrguth <jacobguth96@gmail.com>
2021-09-19 12:22:47 -0500

    poetrydb client

---
+++ Guth.PoetryDB/Models/Poem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Guth.PoetryDB.Models
+{
+    public class Poem
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<string> Lines { get; set; }
+    }
+}
---
+++ Guth.PoetryDB/PoetryDBClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+using Guth.PoetryDB.Models;
+
+namespace Guth.PoetryDB
+{
+    public class PoetryDBClient
+    {
+        private readonly Uri _baseUri;
+        private readonly HttpClient _client;
+
+        internal PoetryDBClient(string baseUrl)
+        {
+            _client = new HttpClient();
+            _baseUri = new Uri(baseUrl);
+
+        public async Task<IEnumerable<Poem>> SearchByAuthorAsync(string author)
+        {
+            var res = await _client.GetFromJsonAsync()
+        }
+
+        private async Task<T> ReturnIfSuccessful<T>(HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<T>();
+        }
+
+        private HttpRequestMessage GetBaseRequest()
+            => new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = _baseUri
+            };
+    }
+}
