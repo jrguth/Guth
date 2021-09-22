c5e30dabe23a3fefdc9ff835b101ef848bab71c7
jrguth <jguth@silverwork-solutions.com>
2021-09-22 17:56:34 -0500

    even moar cleanup

--- Guth.Poetry.Web/Startup.cs
+++ Guth.Poetry.Web/Startup.cs
@@ -5,7 +5,6 @@ using Guth.PoetryDB;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +25,6 @@ namespace Guth.Poetry.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -42,7 +39,6 @@ namespace Guth.Poetry.Web
             services.AddBlazoredLocalStorage();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -52,7 +48,6 @@ namespace Guth.Poetry.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
